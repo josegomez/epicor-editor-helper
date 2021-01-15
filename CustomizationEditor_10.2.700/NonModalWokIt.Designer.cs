662b9261dded9b6a09c818af975374d2167e9f86
Jose C Gomez <jose@josecgomez.com>
2021-01-15 09:32:15 -0500

    Added support for 10.2.700

---
+++ CustomizationEditor_10.2.700/NonModalWokIt.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace CustomizationEditor
+{
+    partial class NonModalWokIt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NonModalWokIt));
+            this.btnTracing = new System.Windows.Forms.Button();
+            this.btnDataDic = new System.Windows.Forms.Button();
+            this.btnEP = new System.Windows.Forms.Button();
+            this.btnBAQ = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnDebug = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblCustom = new System.Windows.Forms.Label();
+            this.chkSyncUp = new System.Windows.Forms.CheckBox();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.btnObjectExplorer = new System.Windows.Forms.Button();
+            this.chkAOT = new System.Windows.Forms.CheckBox();
+            this.btnRefs = new System.Windows.Forms.Button();
+            this.btnCodeWizard = new System.Windows.Forms.Button();
+            this.btnDataTools = new System.Windows.Forms.Button();
+            this.btnProcessCalling = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTracing
+            // 
+            this.btnTracing.Location = new System.Drawing.Point(14, 57);
+            this.btnTracing.Name = "btnTracing";
+            this.btnTracing.Size = new System.Drawing.Size(129, 23);
+            this.btnTracing.TabIndex = 0;
+            this.btnTracing.Text = "&Tracing Options";
+            this.btnTracing.UseVisualStyleBackColor = true;
+            this.btnTracing.Click += new System.EventHandler(this.btnTracing_Click);
+            // 
+            // btnDataDic
+            // 
+            this.btnDataDic.Location = new System.Drawing.Point(14, 86);
+            this.btnDataDic.Name = "btnDataDic";
+            this.btnDataDic.Size = new System.Drawing.Size(129, 23);
+            this.btnDataDic.TabIndex = 1;
+            this.btnDataDic.Text = "Data Dictionar&y";
+            this.btnDataDic.UseVisualStyleBackColor = true;
+            this.btnDataDic.Click += new System.EventHandler(this.btnDataDic_Click);
+            // 
+            // btnEP
+            // 
+            this.btnEP.Location = new System.Drawing.Point(14, 115);
+            this.btnEP.Name = "btnEP";
+            this.btnEP.Size = new System.Drawing.Size(129, 23);
+            this.btnEP.TabIndex = 2;
+            this.btnEP.Text = "Extended Properties";
+            this.btnEP.UseVisualStyleBackColor = true;
+            this.btnEP.Click += new System.EventHandler(this.btnEP_Click);
+            // 
+            // btnBAQ
+            // 
+            this.btnBAQ.Location = new System.Drawing.Point(14, 144);
+            this.btnBAQ.Name = "btnBAQ";
+            this.btnBAQ.Size = new System.Drawing.Size(129, 23);
+            this.btnBAQ.TabIndex = 3;
+            this.btnBAQ.Text = "&BAQ Entry";
+            this.btnBAQ.UseVisualStyleBackColor = true;
+            this.btnBAQ.Click += new System.EventHandler(this.btnBAQ_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(100, 237);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(129, 23);
+            this.btnTest.TabIndex = 4;
+            this.btnTest.Text = "Run (Test)";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnDebug
+            // 
+            this.btnDebug.Location = new System.Drawing.Point(100, 266);
+            this.btnDebug.Name = "btnDebug";
+            this.btnDebug.Size = new System.Drawing.Size(129, 23);
+            this.btnDebug.TabIndex = 5;
+            this.btnDebug.Text = "Debug";
+            this.btnDebug.UseVisualStyleBackColor = true;
+            this.btnDebug.Click += new System.EventHandler(this.btnDebug_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(100, 295);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(129, 23);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "&Edit/Run";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // lblCustom
+            // 
+            this.lblCustom.Location = new System.Drawing.Point(11, 9);
+            this.lblCustom.Name = "lblCustom";
+            this.lblCustom.Size = new System.Drawing.Size(292, 45);
+            this.lblCustom.TabIndex = 7;
+            // 
+            // chkSyncUp
+            // 
+            this.chkSyncUp.AutoSize = true;
+            this.chkSyncUp.Location = new System.Drawing.Point(11, 335);
+            this.chkSyncUp.Name = "chkSyncUp";
+            this.chkSyncUp.Size = new System.Drawing.Size(92, 17);
+            this.chkSyncUp.TabIndex = 8;
+            this.chkSyncUp.Text = "Auto Sync Up";
+            this.chkSyncUp.UseVisualStyleBackColor = true;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Location = new System.Drawing.Point(15, 173);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(129, 23);
+            this.btnMenu.TabIndex = 9;
+            this.btnMenu.Text = "&Menu Maintenance";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnObjectExplorer
+            // 
+            this.btnObjectExplorer.Location = new System.Drawing.Point(174, 57);
+            this.btnObjectExplorer.Name = "btnObjectExplorer";
+            this.btnObjectExplorer.Size = new System.Drawing.Size(129, 23);
+            this.btnObjectExplorer.TabIndex = 10;
+            this.btnObjectExplorer.Text = "&Object Explorer";
+            this.btnObjectExplorer.UseVisualStyleBackColor = true;
+            this.btnObjectExplorer.Click += new System.EventHandler(this.btnObjectExplorer_Click);
+            // 
+            // chkAOT
+            // 
+            this.chkAOT.AutoSize = true;
+            this.chkAOT.Location = new System.Drawing.Point(205, 335);
+            this.chkAOT.Name = "chkAOT";
+            this.chkAOT.Size = new System.Drawing.Size(98, 17);
+            this.chkAOT.TabIndex = 11;
+            this.chkAOT.Text = "Always On Top";
+            this.chkAOT.UseVisualStyleBackColor = true;
+            this.chkAOT.CheckedChanged += new System.EventHandler(this.chkAOT_CheckedChanged);
+            // 
+            // btnRefs
+            // 
+            this.btnRefs.Location = new System.Drawing.Point(175, 86);
+            this.btnRefs.Name = "btnRefs";
+            this.btnRefs.Size = new System.Drawing.Size(128, 23);
+            this.btnRefs.TabIndex = 12;
+            this.btnRefs.Text = "&Reference Manager&";
+            this.btnRefs.UseVisualStyleBackColor = true;
+            this.btnRefs.Click += new System.EventHandler(this.btnRefs_Click);
+            // 
+            // btnCodeWizard
+            // 
+            this.btnCodeWizard.Location = new System.Drawing.Point(175, 115);
+            this.btnCodeWizard.Name = "btnCodeWizard";
+            this.btnCodeWizard.Size = new System.Drawing.Size(128, 23);
+            this.btnCodeWizard.TabIndex = 13;
+            this.btnCodeWizard.Text = "&Code Wizard";
+            this.btnCodeWizard.UseVisualStyleBackColor = true;
+            this.btnCodeWizard.Click += new System.EventHandler(this.btnCodeWizard_Click);
+            // 
+            // btnDataTools
+            // 
+            this.btnDataTools.Location = new System.Drawing.Point(175, 144);
+            this.btnDataTools.Name = "btnDataTools";
+            this.btnDataTools.Size = new System.Drawing.Size(128, 23);
+            this.btnDataTools.TabIndex = 14;
+            this.btnDataTools.Text = "&Data Tools";
+            this.btnDataTools.UseVisualStyleBackColor = true;
+            this.btnDataTools.Click += new System.EventHandler(this.btnDataTools_Click);
+            // 
+            // btnProcessCalling
+            // 
+            this.btnProcessCalling.Location = new System.Drawing.Point(14, 202);
+            this.btnProcessCalling.Name = "btnProcessCalling";
+            this.btnProcessCalling.Size = new System.Drawing.Size(129, 23);
+            this.btnProcessCalling.TabIndex = 15;
+            this.btnProcessCalling.Text = "&Process Calling";
+            this.btnProcessCalling.UseVisualStyleBackColor = true;
+            this.btnProcessCalling.Click += new System.EventHandler(this.BtnProcessCalling_Click);
+            // 
+            // NonModalWokIt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 364);
+            this.Controls.Add(this.btnProcessCalling);
+            this.Controls.Add(this.btnDataTools);
+            this.Controls.Add(this.btnCodeWizard);
+            this.Controls.Add(this.btnRefs);
+            this.Controls.Add(this.chkAOT);
+            this.Controls.Add(this.btnObjectExplorer);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.chkSyncUp);
+            this.Controls.Add(this.lblCustom);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDebug);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnBAQ);
+            this.Controls.Add(this.btnEP);
+            this.Controls.Add(this.btnDataDic);
+            this.Controls.Add(this.btnTracing);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NonModalWokIt";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Epicor Customization ToolBox";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NonModalWokIt_FormClosing);
+            this.Load += new System.EventHandler(this.NonModalWokIt_Load);
+            this.Shown += new System.EventHandler(this.NonModalWokIt_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTracing;
+        private System.Windows.Forms.Button btnDataDic;
+        private System.Windows.Forms.Button btnEP;
+        private System.Windows.Forms.Button btnBAQ;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnDebug;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblCustom;
+        private System.Windows.Forms.CheckBox chkSyncUp;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Button btnObjectExplorer;
+        private System.Windows.Forms.CheckBox chkAOT;
+        private System.Windows.Forms.Button btnRefs;
+        private System.Windows.Forms.Button btnCodeWizard;
+        private System.Windows.Forms.Button btnDataTools;
+        private System.Windows.Forms.Button btnProcessCalling;
+    }
+}
